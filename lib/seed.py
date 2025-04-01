df577da96db563aac2568436a3d0e9d282329019
irarubrian <brian.o.iraru@gmail.com>
2025-04-01 23:21:10 +0300

    Added my-folder

---
+++ lib/seed.py
@@ -0,0 +1,50 @@
+from app import app
+from models import db, Hero, Power, HeroPower
+
+def seed():
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+
+        heroes = [
+            Hero(name='Kamala Khan', super_name='Ms. Marvel'),
+            Hero(name='Doreen Green', super_name='Squirrel Girl'),
+            Hero(name='Gwen Stacy', super_name='Spider-Gwen'),
+            Hero(name='Janet Van Dyne', super_name='The Wasp'),
+            Hero(name='Wanda Maximoff', super_name='Scarlet Witch'),
+            Hero(name='Carol Danvers', super_name='Captain Marvel'),
+            Hero(name='Jean Grey', super_name='Dark Phoenix'),
+            Hero(name='Ororo Munroe', super_name='Storm'),
+            Hero(name='Kitty Pryde', super_name='Shadowcat'),
+            Hero(name='Elektra Natchios', super_name='Elektra')
+        ]
+        db.session.add_all(heroes)
+        db.session.commit()
+
+        powers = [
+            Power(name='super strength', description='gives the wielder super-human strengths'),
+            Power(name='flight', description='gives the wielder the ability to fly through the skies at supersonic speed'),
+            Power(name='super human senses', description='allows the wielder to use her senses at a super-human level'),
+            Power(name='elasticity', description='can stretch the human body to extreme lengths')
+        ]
+        db.session.add_all(powers)
+        db.session.commit()
+
+        hero_powers = [
+            HeroPower(hero_id=1, power_id=1, strength='Strong'),
+            HeroPower(hero_id=1, power_id=2, strength='Average'),
+            HeroPower(hero_id=2, power_id=1, strength='Weak'),
+            HeroPower(hero_id=3, power_id=3, strength='Strong'),
+            HeroPower(hero_id=4, power_id=4, strength='Average'),
+            HeroPower(hero_id=5, power_id=1, strength='Strong'),
+            HeroPower(hero_id=6, power_id=2, strength='Weak'),
+            HeroPower(hero_id=7, power_id=3, strength='Average'),
+            HeroPower(hero_id=8, power_id=4, strength='Strong'),
+            HeroPower(hero_id=9, power_id=1, strength='Weak'),
+            HeroPower(hero_id=10, power_id=2, strength='Average')
+        ]
+        db.session.add_all(hero_powers)
+        db.session.commit()
+
+if __name__ == '__main__':
+    seed()
